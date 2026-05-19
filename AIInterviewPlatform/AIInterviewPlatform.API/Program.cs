3779c9e68cf57301e7de531cd7e345a279c6edeb
Tanh <dangvanthaianh@gmail.com>
2026-05-20 00:34:45 +0700

    add some curri inteface test flow repository

--- AIInterviewPlatform/AIInterviewPlatform.API/Program.cs
+++ AIInterviewPlatform/AIInterviewPlatform.API/Program.cs
@@ -1,5 +1,7 @@
 using AIInterviewPlatform.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using AIInterviewPlatform.Application.Interfaces.Repositories;
+using AIInterviewPlatform.Infrastructure.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
